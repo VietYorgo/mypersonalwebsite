32baff7c570d061993d079ee8e0812c09bba2a7b
gr4c34 <195058572+gr4c34@users.noreply.github.com>
2025-01-15 19:47:17 +0800

    main page

--- app.vue
+++ app.vue
@@ -2,7 +2,7 @@
   <DrawerLayout>
     <template v-slot:header>
       <div class="mx-2 flex-1 px-2">
-        <NuxtLink class="font-bold" href="/">Yorgo</NuxtLink>
+        <NuxtLink class="font-bold" href="/">Main Page</NuxtLink>
       </div>
       <div class="flex-none mx-2 px-2">
         <ThemeSelector />

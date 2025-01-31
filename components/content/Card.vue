a1fe767ebe8b53ebdaba803cc3d01095bc1c475e
gr4c34 <195058572+gr4c34@users.noreply.github.com>
2025-01-31 14:19:54 +0800

    more margin

--- components/content/Card.vue
+++ components/content/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card bg-base-200 m-4 shadow-xl">
+  <div class="card bg-base-200 m-6 shadow-xl">
     <div class="card-body">
       <ContentSlot :use="$slots.default" />
       <div class="card-actions justify-end"></div>
